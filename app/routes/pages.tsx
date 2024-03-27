d2d376697edb921116835e154c51d15736a4d36b
ThanhLM <lmt151099@gmail.com>
2024-03-27 16:24:24 +0700

    Update Pages laoder

--- app/routes/pages.tsx
+++ app/routes/pages.tsx
@@ -1,9 +1,17 @@
+import { LoaderFunctionArgs, json } from '@remix-run/node'
 import Layout from '../components/Layout'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader = async ({ request, params, context }: LoaderFunctionArgs) => {
+  return json({ success: 1, data: ['123123123132']})
+}
 
 export default function Pages() {
+  const { data } = useLoaderData<typeof loader>()
   return (
     <Layout>
-      <h1>Pages</h1>
+      <h1>Pages {data}</h1>
+
     </Layout>
   )
 }
