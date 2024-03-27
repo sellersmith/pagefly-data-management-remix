7165f984d04a52cde94b80e9b74a440c46003fbc
ThanhLM <lmt151099@gmail.com>
2024-03-27 15:35:48 +0700

    update Layout components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -27,25 +27,7 @@ export default function Index() {
   return (
     <Layout>
       <div>
-        <h1>Welcome to Remix</h1>
-        <h2 className='text-blue-600 font-extrabold text-5xl'>TailwindCSS Is Working!</h2>
-        <ul>
-          <li>
-            <a target='_blank' href='https://remix.run/tutorials/blog' rel='noreferrer'>
-              15m Quickstart Blog Tutorial
-            </a>
-          </li>
-          <li>
-            <a target='_blank' href='https://remix.run/tutorials/jokes' rel='noreferrer'>
-              Deep Dive Jokes App Tutorial
-            </a>
-          </li>
-          <li>
-            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-              Remix Docs
-            </a>
-          </li>
-        </ul>
+        <h1 className='font-extrabold text-3xl text-sky-600'>Welcome to Remix</h1>
       </div>
     </Layout>
   )
