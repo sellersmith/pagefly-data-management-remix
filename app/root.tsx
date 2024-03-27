beb10111b03c1eafc5f3706a8cf465ee70aa741e
NguyenQuangHuy282002 <huynq2@bravebits.vn>
2024-03-27 10:23:40 +0700

    add db

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,17 @@
-import { LinksFunction } from '@remix-run/node'
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-import stylesheet from '~/tailwind.css'
-
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }]
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
@@ -19,9 +21,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
 
 export default function App() {
-  return <Outlet />
+  return <Outlet />;
 }
