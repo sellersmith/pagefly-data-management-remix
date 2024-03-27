abe1949b133c9950b7a94d5e54014c7539912173
NguyenQuangHuy282002 <huynq2@bravebits.vn>
2024-03-27 10:55:46 +0700

    add db

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import styles from "./app.css";
+import styles from "./styles/app.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
