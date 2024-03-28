03d874ee6bc4e0b80287eea16d2b3a39027233c6
NguyenQuangHuy282002 <huynq2@bravebits.vn>
2024-03-28 08:07:17 +0700

    add ui

--- app/routes/pages.view/route.tsx
+++ app/routes/pages.view/route.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
 import ViewDataPage from "~/views/pages/view";
 
 export const loader = async ({
@@ -7,6 +7,22 @@ export const loader = async ({
   context,
 }: LoaderFunctionArgs) => {
   return json({ success: 1, data: ["123123123132"] });
+};
+
+export const action = async ({
+  request,
+  params,
+  context,
+}: ActionFunctionArgs) => {
+  const formData = Object.fromEntries(await request.formData());
+
+  const data = !!formData &&
+    formData.body && {
+      ...JSON.parse(formData.body as string),
+    };
+  const { pageId, selectedEnvironment } = data;
+
+  return json({ success: true, data });
 };
 
 export default function PagesView() {
--- app/views/pages/view/index.tsx
+++ app/views/pages/view/index.tsx
@@ -1,44 +1,156 @@
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { Fragment } from "react/jsx-runtime";
 import PageHeading from "~/components/PageHeading";
+import { EEnvironments } from "~/constants/enum";
+import { classNames } from "~/utils/classnames";
+import { useHTTP } from "~/utils/use-http";
+
+const environments = [
+  { value: EEnvironments.PRODUCTION, name: "Production" },
+  { value: EEnvironments.RC, name: "RC" },
+  { value: EEnvironments.WIP, name: "WIP" },
+];
 
 export default function ViewDataPage() {
+  const [selectedEnvironment, setSelectedEnvironment] = useState(
+    environments[1]
+  );
+  const [pageId, setPageId] = useState("");
+  const submit = useHTTP();
+
   return (
-    <div>
+    <form>
       <PageHeading title={"View page data"} backAction={{ url: "/pages" }} />
-      <fieldset className="mt-6 bg-white">
-        <legend className="block text-sm font-medium leading-6 text-gray-900">
-          Billing address
-        </legend>
-        <div className="mt-2 -space-y-px rounded-md shadow-sm">
-          <div>
-            <label htmlFor="country" className="sr-only">
-              Country
-            </label>
-            <select
-              id="country"
-              name="country"
-              autoComplete="country-name"
-              className="relative block w-full rounded-none rounded-t-md border-0 bg-transparent py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      <div className="border-b border-gray-900/10 pb-12 mt-10">
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-3">
+            <Listbox
+              value={selectedEnvironment}
+              onChange={setSelectedEnvironment}
             >
-              <option>United States</option>
-              <option>Canada</option>
-              <option>Mexico</option>
-            </select>
+              {({ open }) => (
+                <>
+                  <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
+                    Enviroment
+                  </Listbox.Label>
+                  <div className="relative mt-2">
+                    <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                      <span className="block truncate">
+                        {selectedEnvironment.name}
+                      </span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
+
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        {environments.map((environment) => (
+                          <Listbox.Option
+                            key={environment.value}
+                            className={({ active }) =>
+                              classNames(
+                                active
+                                  ? "bg-indigo-600 text-white"
+                                  : "text-gray-900",
+                                "relative cursor-default select-none py-2 pl-3 pr-9"
+                              )
+                            }
+                            value={environment}
+                          >
+                            {({ selected, active }) => (
+                              <>
+                                <span
+                                  className={classNames(
+                                    selected ? "font-semibold" : "font-normal",
+                                    "block truncate"
+                                  )}
+                                >
+                                  {environment.name}
+                                </span>
+
+                                {selected ? (
+                                  <span
+                                    className={classNames(
+                                      active ? "text-white" : "text-indigo-600",
+                                      "absolute inset-y-0 right-0 flex items-center pr-4"
+                                    )}
+                                  >
+                                    <CheckIcon
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </>
+              )}
+            </Listbox>
           </div>
-          <div>
-            <label htmlFor="postal-code" className="sr-only">
-              ZIP / Postal code
+
+          <div className="col-span-full">
+            <label
+              htmlFor="street-address"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Page ID
             </label>
-            <input
-              type="text"
-              name="postal-code"
-              id="postal-code"
-              autoComplete="postal-code"
-              className="relative block w-full rounded-none rounded-b-md border-0 bg-transparent py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="ZIP / Postal code"
-            />
+            <div className="mt-2">
+              <input
+                onInput={(e) => setPageId((e.target as any).value)}
+                value={pageId}
+                type="text"
+                name="street-address"
+                id="street-address"
+                autoComplete="street-address"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
         </div>
-      </fieldset>
-    </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={async () => {
+            const data = await submit(
+              {
+                body: JSON.stringify({
+                  pageId,
+                  selectedEnvironment,
+                }),
+              },
+              { method: "POST", navigate: false }
+            );
+          }}
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          View
+        </button>
+      </div>
+    </form>
   );
 }
